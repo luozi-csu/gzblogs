9f79ffa24903f2bb4dcc75cd741110f8e0bb6533
luozi-csu <2848411850@qq.com>
2022-12-04 01:09:51 +0800

    [update]修改logger

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 
 	"github.com/luozi-csu/lzblogs/config"
-	"github.com/luozi-csu/lzblogs/utils"
+	"github.com/luozi-csu/lzblogs/utils/logx"
 )
 
 var configFile = flag.String("f", "./config.yaml", "path of the global config file")
@@ -21,13 +21,13 @@ func main() {
 		"debug": 0, "info": 1, "warn": 2, "error": 3, "fatal": 4,
 	}
 
-	utils.InitLogger(levelMap[logLevel], logPath)
+	logx.InitLogger(levelMap[logLevel], logPath)
 
 	fmt.Println(config.CONF)
 
-	utils.Logger.Debugf("this is a debug level message")
-	utils.Logger.Infof("this is a info level message")
-	utils.Logger.Warnf("this is a warn level message")
-	utils.Logger.Errorf("this is a error level message")
-	utils.Logger.Fatalf("this is a fatal level message")
+	logx.Debugf("this is a debug level message")
+	logx.Infof("this is a info level message")
+	logx.Warnf("this is a warn level message")
+	logx.Errorf("this is a error level message")
+	logx.Fatalf("this is a fatal level message")
 }
